f9867858437dd7f37b942aee42539214b0ffe09d
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-09 17:18:16 +0200

    Update ClearPauseMod.java

--- src/main/java/com/clearpause/ClearPauseMod.java
+++ src/main/java/com/clearpause/ClearPauseMod.java
@@ -3,17 +3,12 @@ package com.clearpause;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.ReflectionHelper;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiIngameMenu;
 import net.minecraft.client.gui.GuiOptions;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenOptionsSounds;
 import net.minecraft.client.gui.GuiVideoSettings;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-
-import java.util.List;
 
 @Mod(
     modid = ClearPauseMod.MODID, 
@@ -26,8 +21,6 @@ public class ClearPauseMod {
     public static final String NAME = "Clear Pause Menu";
     public static final String VERSION = "1.0";
 
-    private static final String[] BUTTON_LIST_FIELD = new String[]{"buttonList", "field_146292_n"};
-
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
@@ -36,19 +29,15 @@ public class ClearPauseMod {
     @SubscribeEvent
     public void onDrawScreen(GuiScreenEvent.DrawScreenEvent.Pre event) {
         if (event.gui != null && event.gui.mc != null && event.gui.mc.theWorld != null) {
-            
             String className = event.gui.getClass().getName();
 
-            // Kontrola hlavnych menu
             boolean isStandardMenu = (event.gui instanceof GuiIngameMenu) 
                                   || (event.gui instanceof GuiOptions) 
                                   || (event.gui instanceof GuiVideoSettings)
                                   || (event.gui instanceof GuiScreenOptionsSounds);
 
-            // Cielime len na konfiguračné obrazovky chatu, nie na samotný chat
             boolean isChatSettings = className.contains("ChatSettings") || className.contains("ChatOptions");
 
-            // Kontrola pod-menu
             boolean isSubMenu = isChatSettings
                                || className.contains("Customiz")
                                || className.contains("GuiOption") 
@@ -60,23 +49,13 @@ public class ClearPauseMod {
                                || className.contains("GuiAnimation");
 
             if (isStandardMenu || isSubMenu) {
-                event.setCanceled(true);
-
-                try {
-                    List<?> buttons = ReflectionHelper.getPrivateValue(GuiScreen.class, event.gui, BUTTON_LIST_FIELD);
-                    
-                    if (buttons != null) {
-                        for (Object obj : buttons) {
-                            if (obj instanceof GuiButton) {
-                                GuiButton button = (GuiButton) obj;
-                                if (button.visible) {
-                                    button.drawButton(event.gui.mc, event.mouseX, event.mouseY);
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                // Pre hlavné pauza menu (GuiIngameMenu) zrušíme kreslenie pozadia / zatmenia,
+                // ale pre GuiVideoSettings a iné podmenu necháme prebehnúť štandardný render,
+                // čím sa nevymažú popisky ani posuvníky.
+                if (event.gui instanceof GuiIngameMenu) {
+                    event.gui.drawDefaultBackground();
+                    // Ak chceš úplne priehľadné menu bez tmavého pozadia v pauze,
+                    // môžeš tu pozadie preskočiť alebo ho vykresliť inak.
                 }
             }
         }
