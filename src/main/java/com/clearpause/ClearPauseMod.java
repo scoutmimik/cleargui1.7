3775a8f4123f3460769634006f62f03acac0db8e
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-09 14:30:40 +0200

    Update ClearPauseMod.java

--- src/main/java/com/clearpause/ClearPauseMod.java
+++ src/main/java/com/clearpause/ClearPauseMod.java
@@ -1,1 +1,82 @@
+package com.clearpause;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiIngameMenu;
+import net.minecraft.client.gui.GuiOptions;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiScreenOptionsSounds;
+import net.minecraft.client.gui.GuiVideoSettings;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import java.util.List;
+
+@Mod(
+    modid = ClearPauseMod.MODID, 
+    name = ClearPauseMod.NAME, 
+    version = ClearPauseMod.VERSION, 
+    acceptableRemoteVersions = "*"
+)
+public class ClearPauseMod {
+    public static final String MODID = "clearpause";
+    public static final String NAME = "Clear Pause Menu";
+    public static final String VERSION = "1.0";
+
+    private static final String[] BUTTON_LIST_FIELD = new String[]{"buttonList", "field_146292_n"};
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onDrawScreen(GuiScreenEvent.DrawScreenEvent.Pre event) {
+        if (event.gui != null && event.gui.mc != null && event.gui.mc.theWorld != null) {
+            
+            String className = event.gui.getClass().getName();
+
+            // Kontrola hlavnych menu
+            boolean isStandardMenu = (event.gui instanceof GuiIngameMenu) 
+                                  || (event.gui instanceof GuiOptions) 
+                                  || (event.gui instanceof GuiVideoSettings)
+                                  || (event.gui instanceof GuiScreenOptionsSounds);
+
+            // Cielime len na konfiguračné obrazovky chatu, nie na samotný chat
+            boolean isChatSettings = className.contains("ChatSettings") || className.contains("ChatOptions");
+
+            // Kontrola pod-menu
+            boolean isSubMenu = isChatSettings
+                             || className.contains("Customiz")
+                             || className.contains("GuiOption") 
+                             || className.contains("ScreenOptions")
+                             || className.contains("GuiDetailSettings")
+                             || className.contains("GuiQualitySettings")
+                             || className.contains("GuiPerformanceSettings")
+                             || className.contains("GuiOtherSettings")
+                             || className.contains("GuiAnimation");
+
+            if (isStandardMenu || isSubMenu) {
+                event.setCanceled(true);
+
+                try {
+                    List<GuiButton> buttons = ReflectionHelper.getPrivateValue(GuiScreen.class, event.gui, BUTTON_LIST_FIELD);
+                    
+                    if (buttons != null) {
+                        for (GuiButton button : buttons) {
+                            if (button.visible) {
+                                // V 1.7.10 sa volá func_146112_a (prípadne drawButton v deobfuskávanom prostredí)
+                                button.func_146112_a(event.gui.mc, event.mouseX, event.mouseY);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
