b01df902ec316f6bc60d0e725b1b6368deae41d7
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-09 18:16:55 +0200

    Update ClearPauseMod.java

--- src/main/java/com/clearpause/ClearPauseMod.java
+++ src/main/java/com/clearpause/ClearPauseMod.java
@@ -14,6 +14,7 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 @Mod(
@@ -22,7 +23,7 @@ import java.util.List;
     version = ClearPauseMod.VERSION, 
     acceptableRemoteVersions = "*"
 )
-class ClearPauseMod {
+public class ClearPauseMod {
     public static final String MODID = "clearpause";
     public static final String NAME = "Clear Pause Menu";
     public static final String VERSION = "1.0";
@@ -35,9 +36,11 @@ class ClearPauseMod {
     }
 
     @SubscribeEvent
-    public void onDrawScreenPre(GuiScreenEvent.DrawScreenEvent.Pre event) {
+    public void onDrawScreen(GuiScreenEvent.DrawScreenEvent.Pre event) {
         if (event.gui != null && event.gui.mc != null && event.gui.mc.theWorld != null) {
+            
             String className = event.gui.getClass().getName();
+            boolean isContainer = event.gui instanceof GuiContainer;
 
             boolean isStandardMenu = (event.gui instanceof GuiIngameMenu) 
                                   || (event.gui instanceof GuiOptions) 
@@ -75,20 +78,34 @@ class ClearPauseMod {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }
-        }
-    }
+            } else if (isContainer) {
+                // Pre inventár zrušíme predvolené stmavenie obrazovky, 
+                // ale necháme vykresliť textúru aj s itemmi cez správne vrstvy.
+                event.setCanceled(true);
 
-    // Pre inventáre (GuiContainer) využijeme Post event, kde vieme zamedziť stmaveniu,
-    // prípadne ak chceme odstrániť pozadie pod inventárom bez straty itemov, 
-    // najlepšie je nechať vykresliť GUI a hneď upraviť alpha blending.
-    @SubscribeEvent
-    public void onDrawScreenPost(GuiScreenEvent.DrawScreenEvent.Post event) {
-        if (event.gui instanceof GuiContainer && event.gui.mc != null && event.gui.mc.theWorld != null) {
-            // Tu môžeme pridať dodatočnú úpravu vykreslenia, ak je potrebná, 
-            // avšak pre čisté odstránenie pozadia inventára v 1.7.10 bez asmu/coremodu
-            // je najspoľahlivejšie nechať kontajner bežať štandardne, 
-            // alebo použiť špecifický mixin/coremod. 
+                try {
+                    GuiContainer container = (GuiContainer) event.gui;
+
+                    // 1. Vykreslíme textúru pozadia GUI (rám, sloty)
+                    Method bgLayer = ReflectionHelper.findMethod(GuiContainer.class, container, new String[]{"drawGuiContainerBackgroundLayer", "func_146976_a"}, float.class, int.class, int.class);
+                    bgLayer.setAccessible(true);
+                    bgLayer.invoke(container, event.renderPartialTicks, event.mouseX, event.mouseY);
+
+                    // 2. Vykreslíme itemy v slotoch (zavoláme vykreslenie slotov, ktoré obchádza absenciu RenderItem kontextu)
+                    Method drawSlots = ReflectionHelper.findMethod(GuiScreen.class, container, new String[]{"drawScreen", "func_73863_a"}, int.class, int.class, float.class);
+                    
+                    // Namiesto toho radšej vyvoláme natívne vykreslenie popredia a slotov:
+                    container.drawWorldBackground(255); // Prípadne ošetrenie pre sloty
+                    
+                    // Bezpečné vykreslenie textov a popredia inventára
+                    Method fgLayer = ReflectionHelper.findMethod(GuiContainer.class, container, new String[]{"drawGuiContainerForegroundLayer", "func_146979_h"}, int.class, int.class);
+                    fgLayer.setAccessible(true);
+                    fgLayer.invoke(container, event.mouseX, event.mouseY);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
